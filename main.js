3434765456ce6686e9b30a81098f8f42f0e832c8
Kanwar <80542410+c0d3r-b0t@users.noreply.github.com>
2021-03-24 19:16:44 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+names_array=["Rohit(Dad)","Aman(Mom)","Gunnika(Sister)","Hari Dev(Grandfather)","Honey(Grandmother)","Me(Kanwar)" ]
+function nextslide(){
+    document.getElementById("album").src=names[names_array];
+}
